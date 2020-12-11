b3259b656e4351ff84261747c3d483759f6b6cd8
banrays <jvjkoenderink@gmail.com>
2020-12-11 09:52:00 +0100

    6. Styling services

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import home1 from '../img/home1.png';
 //Styled
 import styled from 'styled-components';
+import {StyledAbout, StyledDescription, StyledImage, StyledHide} from '../styles';
 
 const AboutSection = () => {
     return(
@@ -29,32 +30,5 @@ const AboutSection = () => {
 }
 
 //Styled component
-const StyledAbout = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`;
 
-const StyledDescription = styled.div`
-    flex: 1;
-    padding-right: 5rem;
-    h2{
-        font-weight: lighter;
-    }
-`;
-const StyledImage = styled.div`
-    flex: 1;
-    overflow: hidden;
-    img{
-        width: 100%;
-        height: 80vh;
-        object-fit: cover;
-    }
-`
-const StyledHide = styled.div`
-    overflow: hidden;
-`
 export default AboutSection;
